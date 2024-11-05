ded74d80c96b09d9b9cafefddb71ff92dbc5e7d3
Allen Camp <allenwcamp@gmail.com>
2024-11-04 22:53:15 -0600

    upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//Create introductory message for user.
+//Create three equations that correspond to the combination.
+//Create a dialog box that displays the combination.
+//Add comments throughout the code.
+
+//message to the website user
+
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//combination to the vault using arithmetic operators
+
+const num1 = 5 * 2
+const num2 = 39 + 1
+const num3 = 78 / 2
+
+//combination of the data above
+
+console.log(`${message} ${num1} - ${num2} - ${num3}`);
